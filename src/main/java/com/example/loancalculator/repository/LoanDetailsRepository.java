8c2e722a056d39c9ca71e88563e43d79e17af61d
sarajoGit <jovanovicsara996@gmail.com>
2025-02-23 23:35:48 +0100

    added repository annotation

--- src/main/java/com/example/loancalculator/repository/LoanDetailsRepository.java
+++ src/main/java/com/example/loancalculator/repository/LoanDetailsRepository.java
@@ -2,6 +2,8 @@ package com.example.loancalculator.repository;
 
 import com.example.loancalculator.model.LoanDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LoanDetailsRepository extends JpaRepository<LoanDetails, Integer> {
 }
